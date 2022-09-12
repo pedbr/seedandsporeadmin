5406e3fe1dca8ca00c9fc4e29d2438c993463720
Pedro <pedro.bras@getflowbox.com>
2022-09-12 11:42:22 +0200

    sets up supabase client and connection

--- src/components/Views/ProductsView.tsx
+++ src/components/Views/ProductsView.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { supabase } from '../../supabaseClient'
 
 const ProductsView = () => {
+  const fetchProducts = async () => {
+    const { data, error } = await supabase.from('products').select()
+    console.log(data)
+    console.log(error)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return <div>ProductsView</div>
 }
 
