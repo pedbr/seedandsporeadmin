26058ec28ae992042b64a9e7f26c4df6ad7c6150
Pedro <pedro@worldfavor.com>
2022-12-15 21:36:50 +0000

    adds configs object

---
+++ src/api/secrets.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBc5KLsEVNT5EgjPx0HW2hJqSO5F4fLw2Q',
+  authDomain: 'seedandsporept.firebaseapp.com',
+  projectId: 'seedandsporept',
+  storageBucket: 'seedandsporept.appspot.com',
+  messagingSenderId: '479584535051',
+  appId: '1:479584535051:web:c86233e54f2b2886407d0e',
+}
